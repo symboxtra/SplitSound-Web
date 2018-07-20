eca98dac2d8d3d591265e12f667b44e6949433cb
polak69 <epituch@purdue.edu>
2018-07-20 15:21:07 -0500

    added an about us page, just has names for now, dont know what to put there yet

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,11 @@
             <p> For when you do not want to run a 200 foot audio cable throughout your house.</p>
             <p> A free alternative to wireless bluetooth wifi 2.4 ghz headphones.</p>
         </div>
-        <img src="../assets/images/example1.jpg" alt="Oops" class="tallPic1">
-        <img src="../assets/images/example1.jpg" alt="Oops" class="tallPic2">
-        <img src="../assets/images/example1.jpg" alt="Oops" class="tallPic3">
-        <img src="../assets/images/example2.png" alt="Oops" class="widePic">
+        <img src="../assets/images/example1.jpg" alt="Mobile PlaceHolder" class="tallPic1">
+        <img src="../assets/images/example1.jpg" alt="Mobile PlaceHolder" class="tallPic2">
+        <img src="../assets/images/example1.jpg" alt="Mobile PlaceHolder" class="tallPic3">
+        <img src="../assets/images/example2.png" alt="Desktop PlaceHolder" class="widePic1">
+        <img src="../assets/images/example2.png" alt="Desktop PlaceHolder" class="widePic2">
     </div>
 </template>
 
