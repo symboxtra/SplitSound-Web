9cf3ef8f13d80a9c6581e1c81929952ac134c646
polak69 <epituch@purdue.edu>
2018-07-19 21:59:30 -0500

    added a download page and components to dynamically render based on OS, planning to fix the images tommorow, girlfriend called

--- src/components/Download.vue
+++ src/components/Download.vue
@@ -1,15 +1,72 @@
 <template>
-    <div class="content">
-        <h1 class="center">Download</h1>
+    <div class="gridWrapper2">
+        <div class="downloadSection">
+            <h1>Get SplitSound for {{ OSName }} </h1>
+            <p>SplitSound is Symboxtra's revolutionary flagship audio sharing product. Get it now!</p>
+            <button class="btn btn-dark btn-main">Download</button>
+        </div>
+        <download-element class="downloadElement1" :osname="RenderedOSs[0]">
+        </download-element>
+        <download-element class="downloadElement2" :osname="RenderedOSs[1]">
+        </download-element>
+        <download-element class="downloadElement3" :osname="RenderedOSs[2]">
+        </download-element>
+        <download-element class="downloadElement4" :osname="RenderedOSs[3]">
+        </download-element>
     </div>
 </template>
 
 <script>
+    import DownloadElement from "./DownloadElement"
     export default {
-        name: "Download"
+        name: "Download",
+        data() {
+            return {}
+        },
+        components:{
+            DownloadElement
+        },
+        computed: {
+            OSName() {
+                return this.getOS();
+            },
+            RenderedOSs() {
+                let osa = ['Windows','Mac OS','Linux','Android','iOS'];
+                let i = osa.indexOf(this.getOS());
+                if (i !== -1) {
+                    osa.splice(i, 1);
+                }
+                return osa;
+            }
+        },
+        methods: {
+            getOS() {
+                //shamelessly stolen from https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+                let userAgent = window.navigator.userAgent,
+                    platform = window.navigator.platform,
+                    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+                    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+                    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+                    os = null;
+
+                if (macosPlatforms.indexOf(platform) !== -1) {
+                    os = 'Mac OS';
+                } else if (iosPlatforms.indexOf(platform) !== -1) {
+                    os = 'iOS';
+                } else if (windowsPlatforms.indexOf(platform) !== -1) {
+                    os = 'Windows';
+                } else if (/Android/.test(userAgent)) {
+                    os = 'Android';
+                } else if (!os && /Linux/.test(platform)) {
+                    os = 'Linux';
+                }
+
+                return os;
+            }
+        }
     }
 </script>
 
 <style scoped>
-    @import "../assets/css/components.css";
+    @import "../assets/css/components-style/download-style.css";
 </style>
---
+++ src/components/DownloadElement.vue
@@ -0,0 +1,62 @@
+<template>
+    <div :class="{ android: OSName === 'Android', ios: OSName === 'iOS', linux: OSName === 'Linux', mac: OSName === 'Mac OS', windows: OSName === 'Windows'}">
+        <div><h1>{{ OSName }}</h1>
+            <button class="btn btn-dark">Download</button>
+        </div>
+        <img src="../assets/images/example2.png" alt="Desktop PlaceHolder" v-if="isDesktop">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DownloadElement",
+        props: ['osname'],
+        computed: {
+            OSName() {
+                return this.osname;
+            }
+        },
+        methods:{
+            isDesktop(){
+                return (this.OSName() === 'Linux' | this.OSName() === 'Mac OS'| this.OSName() === 'Windows');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .btn {
+        font-size: 25px;
+    }
+
+    img {
+        padding-top: 5%;
+        max-width: 70%;
+        max-height: 70%;
+    }
+
+    .android {
+        background-color: #A4C639;
+        /*android green*/
+    }
+
+    .ios {
+        background-color: #8E8E93;
+        /*ios gray*/
+    }
+
+    .linux {
+        background-color: #FFFF00;
+        /*could not find one for linux*/
+    }
+
+    .mac {
+        background-color: #8E8E91;
+        /*mac gray*/
+    }
+
+    .windows {
+        background-color: #2D89Ef;
+        /*Windows 10 blue*/
+    }
+</style>
