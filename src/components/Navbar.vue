626aab1c8ec1777e8668c50893a2b360c8204b9c
polak69 <epituch@purdue.edu>
2018-07-26 14:44:56 -0500

    added hover stuff to the footer elements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,37 +13,6 @@
                 </b-navbar-nav>
             </b-collapse>
         </b-navbar>
-        <!--<nav class="navbar fixed-top navbar-expand-lg navbar-dark">-->
-        <!--&lt;!&ndash;<a class="navbar-brand" href="#" @click="">SplitSound</a>&ndash;&gt;-->
-        <!--<router-link class="navbar-brand" to="home">SplitSound</router-link>-->
-        <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"-->
-        <!--aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-        <!--<span class="navbar-toggler-icon"></span>-->
-        <!--</button>-->
-
-        <!--<div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-        <!--<ul class="navbar-nav ml-auto">-->
-        <!--<li class="nav-item active ml-auto">-->
-        <!--<router-link to="home" class="nav-link navbar-collapse">Home</router-link>-->
-        <!--</li>-->
-        <!--<li class="nav-item ml-auto">-->
-        <!--<router-link to="features" class="nav-link">Features</router-link>-->
-        <!--&lt;!&ndash;<a class="nav-link" href="#">Learn More</a>&ndash;&gt;-->
-        <!--</li>-->
-        <!--<li class="navbar-nav ml-auto">-->
-        <!--&lt;!&ndash;<a href="#" class="nav-link">Download</a>&ndash;&gt;-->
-        <!--<router-link to="download" class="nav-link">Download</router-link>-->
-        <!--</li>-->
-        <!--<li class="navbar-nav ml-auto">-->
-        <!--&lt;!&ndash;<a href="#" class="nav-link">About Us</a>&ndash;&gt;-->
-        <!--<router-link to="about-us" class="nav-link">About Us</router-link>-->
-        <!--</li>-->
-        <!--<li class="navbar-nav ml-auto">-->
-        <!--<a href="https://github.com/symboxtra" class="nav-link" target="_blank">Github</a>-->
-        <!--</li>-->
-        <!--</ul>-->
-        <!--</div>-->
-        <!--</nav>-->
     </div>
 </template>
 
