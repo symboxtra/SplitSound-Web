678600de241db4d3caa573ce328eb06110d2e4f5
polak69 <epituch@purdue.edu>
2018-07-26 11:58:08 -0500

    added a responsive carousel, some aspects still need fixing but I need assistance in how to

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,43 +5,54 @@
             <p> For when you do not want to run a 200 foot audio cable throughout your house.</p>
             <p> A free alternative to wireless bluetooth wifi 2.4 ghz headphones.</p>
         </div>
-        <b-carousel
-                class="carousel"
-                controls
-                indicators
-                background="#ababab"
-                :interval="4000"
-                img-width="1024"
-                img-height="480">
-            <b-carousel-slide
-                    img-width="1024"
-                    img-height="480"
-                    :img-src="require('../assets/images/example1.jpg')">
-                >
-            </b-carousel-slide>
-            <b-carousel-slide
-                    :img-src="require('../assets/images/example1.jpg')">
-                >
-            </b-carousel-slide>
-            <b-carousel-slide
-                    :img-src="require('../assets/images/example1.jpg')">
-                >
-            </b-carousel-slide>
-            <b-carousel-slide
-                    :img-src="require('../assets/images/example2.png')">
-                >
-            </b-carousel-slide>
-            <b-carousel-slide
-                    :img-src="require('../assets/images/example2.png')">
-                >
-            </b-carousel-slide>
-        </b-carousel>
+        <v-carousel class="carousel">
+            <v-carousel-item>
+                <img src="../assets/images/example1.jpg" alt="Mobile PlaceHolder" class="responsiveMImg responsiveImg">
+            </v-carousel-item>
+            <v-carousel-item>
+                <img src="../assets/images/example1.jpg" alt="Mobile PlaceHolder" class="responsiveMImg responsiveImg">
+            </v-carousel-item>
+            <v-carousel-item>
+                <img src="../assets/images/example2.png" alt="Desktop PlaceHolder" class="responsiveDImg responsiveImg">
+            </v-carousel-item>
+            <v-carousel-item>
+                <img src="../assets/images/example2.png" alt="Desktop PlaceHolder" class="responsiveDImg responsiveImg">
+            </v-carousel-item>
+        </v-carousel>
+        <!--<b-carousel-->
+        <!--class="carousel"-->
+        <!--controls-->
+        <!--indicators-->
+        <!--background="transparent"-->
+        <!--:interval="4000"-->
+        <!--img-width="1024"-->
+        <!--img-height="480">-->
+        <!--<b-carousel-slide-->
+        <!--:img-src="require('../assets/images/example1.jpg')">-->
+        <!--&gt;-->
+        <!--</b-carousel-slide>-->
+        <!--<b-carousel-slide-->
+        <!--:img-src="require('../assets/images/example1.jpg')">-->
+        <!--&gt;-->
+        <!--</b-carousel-slide>-->
+        <!--<b-carousel-slide-->
+        <!--:img-src="require('../assets/images/example1.jpg')">-->
+        <!--&gt;-->
+        <!--</b-carousel-slide>-->
+        <!--<b-carousel-slide-->
+        <!--:img-src="require('../assets/images/example2.png')">-->
+        <!--</b-carousel-slide>-->
+        <!--<b-carousel-slide-->
+        <!--:img-src="require('../assets/images/example2.png')">-->
+        <!--</b-carousel-slide>-->
+        <!--</b-carousel>-->
     </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        props: []
     }
 </script>
 
